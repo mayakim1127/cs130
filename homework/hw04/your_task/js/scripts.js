569d7e2ad0808a7673911838e60269bd69afac5b
mayakim1127 <92958350+mayakim1127@users.noreply.github.com>
2022-05-25 16:39:41 -0500

    pushpush\

--- homework/hw04/your_task/js/scripts.js
+++ homework/hw04/your_task/js/scripts.js
@@ -21,46 +21,76 @@ const getTracks = (term) => {
         get tracks from spotify based on the search term
         "${term}" and load them into the #tracks section 
         of the DOM...`);
+    document.querySelector('#tracks').innerHTML = "";
+    console.log('about to fetch!');
+    fetch('https://www.apitutor.org/spotify/simple/v1/search?type=track&limit=5&q='+ term)
+        .then(response => response.json())
+        .then(tracks => {
+            console.log(tracks);
+            counter = 0;
+            if (tracks.length === 0) {
+                document.querySelector('#tracks').innerHTML =
+                `<p>No tracks found for "${term}"</p>`
+            }
+            for (const track of tracks) {
+                    document.querySelector('#tracks').innerHTML +=
+                        `<button class="track-item preview" data-preview-track="${track.preview_url}">
+                            <img src="${track.album.image_url}" alt = "Image of Searched Track">
+                            <i class="fas play-track fa-play" aria-hidden="true"></i>
+                            <div class="label">
+                                <h2>${track.name}</h2>
+                                <p>
+                                    ${track.artist.name}
+                                </p>
+                            </div>
+                        </button>
+                `;
+                    counter += 1;
+                    if (counter >= 5){
+                     break;   
+                    }
+            }
+        });
 };
 
 const getAlbums = (term) => {
+    document.querySelector('#albums').innerHTML = "";
     console.log(`
         get albums from spotify based on the search term
         "${term}" and load them into the #albums section 
         of the DOM...`);
-    const elem = document.querySelector('#albums');
-    elem.innerHTML = "";
-        
-    fetch(baseURL + "?type=album&q=" + term)
-                .then(response => response.json())
-                .then(data => {
-                    if(data.length > 0) {
-                        const firstAlbum = data[0]
-                        elem.innerHTML += getAlbumHTML(firstAlbum)
-                    }
-                })
-            
-    };
-    
-    const getAlbumHTML = (data) => {
-        if(!data.image_url){
-    
+    console.log("about to fetch!");
+    fetch("https://www.apitutor.org/spotify/simple/v1/search?type=album&q=" + term)
+    .then(response => response.json())
+    .then(albums => {
+        console.log(albums);
+        if (albums.length == 0){
+            document.querySelector('#albums').innerHTML = `<p>No albums found for "${term}"</p>`;
         }
+        counter = 0;
+        for (const album of albums){
+            document.querySelector('#albums').innerHTML += `
+                <section class = "album-card" id = "${album.id}">
+                <div>
+                    <img src = "${album.image_url}" alt = "Image of Searched Album" > 
+                    <h2> ${album.name} </h2>
+                    <div class = "footer">
+                        <a href="${album.spotify_url}" target ="_blank">
+                        view on spotify
+                        </a>
+                    </div>
+                </div>
+            </section>
+            `;
+            console.log(`<p>${album.name}</p>`);
+            counter += 1;
+            if (counter >= 100){
+                break;
+            }
+        }
+    })
+};
     
-        return `
-        <section class="album-card" id="${data.id}">
-        <div>
-            <img src="${data.image_url}">
-            <h2>${data.name}</h2>
-            <div class="footer">
-                <a href="${data.spotify_url}" target="_blank">
-                view on spotify
-                </a>
-            </div>
-        </div>
-    </section>
-        `
-    };
 
 const getArtist = (term) => {
     console.log(`
@@ -88,7 +118,7 @@ const getArtistHTML = (data) => {
     return `
     <section class="artist-card" id ="${data.id}">
     <div>
-        <img src="${data.image_url}">
+        <img src="${data.image_url}"alt = "Image of Searched Artist">
         <h2>${data.name}</h2>
         <div class="footer">
             <a href="${data.spotify_url}" target="_blank">
