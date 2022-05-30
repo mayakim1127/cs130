d2bebecacb991f3965fa797331809971da09284c
mayakim1127 <92958350+mayakim1127@users.noreply.github.com>
2022-05-29 20:58:15 -0500

    push!

--- Final Project/index.js
+++ Final Project/index.js
@@ -1,47 +1,40 @@
 
- const images = []
+ "use strict"
 
-function initScreen() {
-    images.forEach((image, idx) => {
-        document.querySelector('.cards').innerHTML += `
-        <li class="card">
-            <button onclick="showImage(event)" class="image" 
-                style="background-image:url('${image}')"
-                data-index=${idx}"
-                aria-label="Displays image ${idx} in the main panel."></button>
-        </li>`;
-    });
-    document.querySelector('.next').onclick = showNext;
-    document.querySelector('.prev').onclick = showPrev;
-    document.querySelector('.featured_image').onclick = showNext;
-}
 
-let currentIndex = 0;
-
-const showImage = (ev) => {
-   const elem = ev.currentTarget;
-   currentIndex = parseInt(elem.dataset.index);
-   console.log("currentIndex:", currentIndex);
-   document.querySelector('.featured_image').style.backgroundImage = elem.style.backgroundImage
-};
-
-const showNext = (ev) => {
-    currentIndex += 1;
-    console.log("currentIndex:", currentIndex);
-    console.log(images[currentIndex])
-    let image = "url('" + images[currentIndex %8] +"')"
-    console.log(image)
-    document.querySelector('.featured_image').style.backgroundImage = image
-};
-
-const showPrev = (ev) => {
-    currentIndex -= 1;
-    if (currentIndex < 0) currentIndex += 8
-    console.log("currentIndex:", currentIndex);
-    console.log(images[currentIndex])
-    let image = "url('" + images[currentIndex %8] +"')"
-    console.log(image)
-    document.querySelector('.featured_image').style.backgroundImage = image
-};
-
-initScreen();
+ const carouselButtons = document.querySelectorAll(".carousel-btn");
+ const carouselButtonLeft = document.querySelector(".btn-left");
+ const carouselButtonRight = document.querySelector(".btn-right");
+ const carouselImage1 = document.querySelector(".carousel-image-1");
+ const carouselImage2 = document.querySelector(".carousel-image-2");
+ const carouselImage3 = document.querySelector(".carousel-image-3");
+ const numberOfImages = document.querySelectorAll(".carousel-image").length;
+ let imageIndex = 1;
+ let translateX = 0;
+ 
+ carouselButtonLeft.addEventListener("click", slideLeft);
+ carouselButtonRight.addEventListener("click", slideRight);
+ 
+ function slideLeft(){
+     if (imageIndex !== 1){
+         imageIndex--;
+         translateX += 400;
+         carouselImage1.style.transform = `translateX(${translateX}px)`;
+         carouselImage2.style.transform = `translateX(${translateX}px)`;
+         carouselImage3.style.transform = `translateX(${translateX}px)`;
+     }
+     
+ }
+ 
+ function slideRight(){
+     if (imageIndex !== numberOfImages){
+         imageIndex++;
+         translateX -= 400;
+         carouselImage1.style.transform = `translateX(${translateX}px)`;
+         carouselImage2.style.transform = `translateX(${translateX}px)`;
+         carouselImage3.style.transform = `translateX(${translateX}px)`;
+     }
+     
+ }
+ 
+ 
